cee1675d2311cd444c26e04a6828afb42a883954
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-15 11:22:21 +0200

    start

--- src/main.js
+++ src/main.js
@@ -1,11 +1,33 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import { createStore } from "vuex";
 
-import './assets/main.css'
+import App from "./App.vue";
 
-const app = createApp(App)
+const store = createStore({
+  state() {
+    return {
+      counter: 0,
+    };
+  },
+  mutations: {
+    increment(state) {
+      state.counter++;
+    },
+  },
+  actions: {
+    increment(context) {
+      context.commit("increment");
+    },
+  },
+  getters: {
+    counter(state) {
+      return state.counter;
+    },
+  },
+});
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(store);
+
+app.mount("#app");
